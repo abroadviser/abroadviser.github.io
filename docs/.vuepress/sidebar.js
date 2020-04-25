4f1ed2f0f1ed6c07125323a90f44c613951b84c8
Jake Kim <jake.kim@weperson.com>
2020-04-25 20:03:55 +0900

    menus

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -8,20 +8,42 @@ module.exports = {
         '/guide/general/Lotus-Lantern-Festival-Seoul-2019',
         '/guide/general/Introduction-to-Korean-Cuisine'
       ]
+    },
+    {
+      title: 'Daily Life',
+      children: []
+    },
+    {
+      title: 'Accommodations',
+      children: []
+    },
+    {
+      title: 'Finance',
+      children: []
+    },
+    {
+      title: 'Administration',
+      children: []
+    },
+    {
+      title: 'Health',
+      children: []
+    },
+    {
+      title: 'Work & Business',
+      children: []
+    },
+    {
+      title: 'Transportation',
+      children: []
+    },
+    {
+      title: 'Culture',
+      children: []
+    },
+    {
+      title: 'Education',
+      children: []
     }
   ]
 }
-
-/*
-Culture
-Finance
-Transportation
-Daily Life
-Accommodations
-Administration
-Education
-Lifestyle
-Health
-Leisure
-Work & Business
-*/
--- docs/guide/general/Introduction-to-Korean-Cuisine.md
+++ docs/guide/general/Introduction-to-Korean-Cuisine.md
@@ -1,5 +1,3 @@
 # Introduction to Korean Cuisine
 
 <youtube src="https://www.youtube.com/embed/JdwgtMB4uQc" />
-
-additional text
