526bdca41df768af880afa4bc5830b70b1c684bf
Jake Kim <jake.kim@weperson.com>
2020-04-25 17:53:34 +0900

    style

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,18 @@
 module.exports = {
   title: 'ABROADVISER',
   description: 'easy life in a foreign country',
-  dest: 'dist',
+  themeConfig: {
+    logo: '/icons/icon-32x32.png',
+    nav: [
+      { text: 'Guide', link: '/guide/' },
+      { text: 'Youtube', link: 'https://www.youtube.com/channel/UCt3w8KxLt_FoVp0jogfdX3Q' }
+    ],
+    lastUpdated: 'Last Updated'
+  },
   head: [
     ['link', { rel: 'icon', href: '/icon.png' }],
     ['link', { rel: 'manifest', href: '/manifest.webmanifest' }]
   ],
-  serviceWorker: { updatePopup: true }
+  serviceWorker: { updatePopup: true },
+  dest: 'dist'
 }
