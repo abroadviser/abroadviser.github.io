193849e9c27f7b3416a6d170467cecdf5de18a90
Jake Kim <jake.kim@weperson.com>
2020-04-26 22:18:48 +0900

    plugins

---
+++ docs/.vuepress/components/comments.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page-edit">
+    <Disqus :key="attrs.identifier" v-bind="attrs" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'comments',
+  computed: {
+    attrs () {
+      const { origin, pathname } = window.location
+      const url = origin + pathname
+      const { title } = this.$page
+      console.log('attrs', url, pathname, title)
+      return { shortname: 'abroadviser', url, identifier: pathname, title }
+    }
+  },
+  watch: {
+    'attrs.identifier' (identifier) {
+      if (window && window.DISQUS) {
+        console.log('watch', identifier)
+        window.DISQUS.reset({ reload: true, config () { this.page.identifier = identifier } })
+      }
+    }
+  }
+}
+</script>
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,11 +14,26 @@ module.exports = {
   },
   head: [
     ['link', { rel: 'icon', href: '/icon.png' }],
-    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }]
+    ['link', { rel: 'manifest', href: '/manifest.webmanifest' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/icon-16x16.png', color: '#3eaf7c' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
+    ['meta', { name: 'naver-site-verification', content: '9f6fd9374b76e620a5d27ecde3ad0ce5ff285294' }],
+    ['meta', { name: 'google-site-verification', content: '_foyyoJ5pPvEdoIZjTVELtmkAh8b0pZ83LXTC4zfHXw' }]
   ],
-  serviceWorker: { updatePopup: true },
   dest: 'dist',
-  extraWatchFiles: [
-    '.vuepress/sidebar.js'
-  ]  
+  extraWatchFiles: ['.vuepress/sidebar.js'],
+  plugins: [
+    ['@vuepress/back-to-top'],
+    ['@vuepress/plugin-medium-zoom'],
+    ['@vuepress/google-analytics', { 'ga': 'UA-144184493-1' }],
+    ['@vuepress/pwa', { serviceWorker: true, updatePopup: true }],
+    ['sitemap', { hostname: 'https://www.abroadviser.com' }],
+    ['seo'],
+    ['disqus']
+  ]
 }
